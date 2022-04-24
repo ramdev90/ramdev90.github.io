b5e3284f592b490d8b3bbf1f273223e1333ca66e
Ramdev <ramdevrathod90@gmail.com>
2022-04-24 12:03:35 +0530

    commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,15 @@ import { AlertComponent } from "./alert/alert.component";
 import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
 import { PlaceholderDirective } from "./placeholder/placeholder.directive";
 import { DropdownDirective } from "./dropdown.directive";
+import { CustomDatepickerComponent } from './custom-datepicker/custom-datepicker.component';
 
 @NgModule({
     declarations: [
         AlertComponent,
         PlaceholderDirective,
         LoadingSpinnerComponent,
-        DropdownDirective
+        DropdownDirective,
+        CustomDatepickerComponent
 
     ],
     imports: [
