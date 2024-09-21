714437d445a87b6535227e9dc38e4929b000723c
Ramdev <work.ramdev@gmail.com>
2024-09-21 11:44:51 +0530

    version update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -73,15 +73,15 @@ export class AuthService {
   }
 
   autoLogin() {
-    const userData: { email: string, id: string, _token: string, _tokenExpirationData: string } = JSON.parse(localStorage.getItem('userData') || '{}');
+    const userData: { email: string, id: string, _token: string, _tokenExpirationDate: string } = JSON.parse(localStorage.getItem('userData') || '{}');
     if (!userData) {
       return;
     }
     else {
-      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationData));
+      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
       if (loadedUser.token) {
         this.user.next(loadedUser);
-        const expirationDuration = new Date(userData._tokenExpirationData).getTime() - new Date().getTime();
+        const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
         this.autoLogout(expirationDuration);
       }
     }
